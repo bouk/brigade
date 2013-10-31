488d431dbcdcc2ec0bb646999f8b92f147327097
Bouke van der Bijl <boukevanderbijl@gmail.com>
2013-10-31 17:02:52 +0100

    Use Put Copy for copying files

--- files.go
+++ files.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/bouk/goamz/s3"
 	"log"
-	"strconv"
 	"sync/atomic"
 )
 
@@ -23,35 +22,11 @@ func (s *S3Connection) copyFile(key string) {
 		atomic.AddInt64(&Stats.working, -1)
 	}()
 
-	source, err := s.Source.GetResponse(key)
+	err := s.Destination.PutHeader(key, []byte{}, map[string][]string{
+		"x-amz-copy-source": {Config.Source + "/" + key},
+	}, s3.PublicRead)
+
 	if err != nil {
 		addError(err)
-		return
-	}
-	defer source.Body.Close()
-
-	if source.Header["Content-Length"] == nil || len(source.Header["Content-Length"]) != 1 {
-		log.Printf("Missing Content-Length for key %s\n", key)
-		return
-	}
-
-	if source.Header["Content-Type"] == nil || len(source.Header["Content-Type"]) != 1 {
-		log.Printf("Missing Content-Type for key %s\n", key)
-		return
-	}
-
-	length, err := strconv.ParseInt(source.Header["Content-Length"][0], 10, 64)
-	if err != nil {
-		addError(err)
-		return
-	}
-
-	mime := source.Header["Content-Type"][0]
-
-	err = s.Destination.PutReader(key, source.Body, length, mime, s3.PublicRead)
-	if err != nil {
-		addError(err)
-	} else {
-		atomic.AddInt64(&Stats.bytes, length)
 	}
 }
